c09ce58cc92c1a6867889236e5d8895a3531ab4f
Vincent <yc660@duke.edu>
2025-03-07 15:11:58 -0500

    testing CI

---
+++ app/src/test/java/factorserver/FactorIOTest.java
@@ -0,0 +1,13 @@
+package factorserver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class FactorIOTest {
+  @Test
+  public void test_() {
+
+  }
+
+}
