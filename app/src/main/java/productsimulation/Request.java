cc8f847b7097bcbb7adbd3c575db10131a5b525c
Vincent <yc660@duke.edu>
2025-03-10 10:33:44 -0400

    comment out ENUM

--- app/src/main/java/productsimulation/Request.java
+++ app/src/main/java/productsimulation/Request.java
@@ -1,22 +1,18 @@
 package productionsimulation;
 
-public enum RequestStatus {
-  WAITING, WORKING, READY;
-}
-
 public class Request {
 
   public int id;
   private final String ingredient;
   private final Recipe recipe;
   private final Building requester;
-  private RequestStatus status;
+  //private RequestStatus status;
 
   public Request(String ingredient, Recipe recipe, Building requester) {
     this.ingredient = ingredient;
     this.recipe = recipe;
     this.requester = requester;
-    this.status = RequestStatus.WAITING;
+
   }
 
   public static int nextRequestId() {
