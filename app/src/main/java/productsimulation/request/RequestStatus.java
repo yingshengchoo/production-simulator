976d5b4e2bff9e94711aba6d518d7c35d00632f0
Mud12 <Mud121025@gmail.com>
2025-03-16 20:40:01 -0400

    Convert RequestStatus from enum to class for clover testing

--- app/src/main/java/productsimulation/request/RequestStatus.java
+++ app/src/main/java/productsimulation/request/RequestStatus.java
@@ -1,7 +1,32 @@
 package productsimulation.request;
 
-public enum RequestStatus {
-    WAITING,
-    READY,
-    WORKING
+public class RequestStatus {
+    public static final RequestStatus WAITING = new RequestStatus("WAITING");
+    public static final RequestStatus READY = new RequestStatus("READY");
+    public static final RequestStatus WORKING = new RequestStatus("WORKING");
+
+    private final String name;
+
+    private RequestStatus(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        RequestStatus that = (RequestStatus) obj;
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
+
