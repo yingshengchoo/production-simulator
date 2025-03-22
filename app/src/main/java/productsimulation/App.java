0f073d99fca64c04346c1a4cdaf6d9be47a81b45
Jiakai Luo <jiakai.luo@duke.edu>
2025-03-22 13:02:30 -0400

    move e2e to test, placeholder for initialize() in App

--- app/src/main/java/productsimulation/App.java
+++ app/src/main/java/productsimulation/App.java
@@ -1,66 +1,19 @@
 package productsimulation;
 
-import productsimulation.command.*;
-import productsimulation.model.*;
-import productsimulation.request.OneTimeServePolicy;
-import productsimulation.request.Request;
-import productsimulation.request.ServePolicy;
-import productsimulation.sourcePolicy.SoleSourcePolicy;
-import productsimulation.sourcePolicy.SourcePolicy;
-
-import java.util.*;
-
 
 public class App {
+    private static void initialize() {
+        Log.debugLog("initializing");
+        // step 1: read the json, apply check rules
 
-    private static void minimalE2E() {
-        // step 1: setup
-        // skip parsing the recipe
-        Recipe woodMineRecipe = new Recipe(1, new LinkedHashMap<String, Integer>(), "wood");
-        LinkedHashMap<String, Integer> woodenSwordIngredients = new LinkedHashMap<>();
-        woodenSwordIngredients.put("wood", 2);
-        Recipe woodSwordRecipe = new Recipe(1, woodenSwordIngredients, "wooden_sword");
-        // skip parsing the type
-        Map<String, Recipe> woodMineRecipes = new HashMap<>();
-        woodMineRecipes.put("wood", woodMineRecipe);
-        FactoryType woodMineType = new FactoryType("wood_mine", woodMineRecipes);
-        Map<String, Recipe> woodSwordRecipes = new HashMap<>();
-        woodSwordRecipes.put("wooden_sword", woodSwordRecipe);
-        FactoryType woodSwordType = new FactoryType("sword_factory", woodSwordRecipes);
-        // skip serving policy and request policy
-        SourcePolicy soleSourcePolicy = new SoleSourcePolicy();
-        ServePolicy oneTimeServePolicy = new OneTimeServePolicy();
-        // skip parsing the building
-        Mine woodMine = new Mine("FirstWoodMine", woodMineType,
-                new ArrayList<Building>(), soleSourcePolicy, oneTimeServePolicy);
-        List<Building> sources = new ArrayList<>();
-        sources.add(woodMine);
-        Factory woodSwordFactory = new Factory("FirstWoodenSwordFactory", woodSwordType,
-                sources, soleSourcePolicy, oneTimeServePolicy);
+        // step 2: create data model
 
-        // step 2: get user prompt and print results
-        // skip parsing the command
-        // skip save/load
-        // skip finish command
-        LogicTime logicTime = LogicTime.getInstance();
-        logicTime.addObservers(woodMine);
-        logicTime.addObservers(woodSwordFactory);
-        RequestBroadcaster requestBroadcaster = RequestBroadcaster.getInstance();
-        requestBroadcaster.addRecipes(woodMineRecipe);
-        requestBroadcaster.addRecipes(woodSwordRecipe);
-        requestBroadcaster.addBuildings(woodMine);
-        requestBroadcaster.addBuildings(woodSwordFactory);
+        // step 3: create LogicTime and RequestBroadcaster
 
-        RequestCommand requestCommand = new RequestCommand("wooden_sword", "FirstWoodenSwordFactory");
-        requestCommand.execute();
-        StepCommand stepCommand = new StepCommand(1);
-        stepCommand.execute();
-        stepCommand.execute();
-        stepCommand.execute();
-        stepCommand.execute();
+        // step 4: enter interaction phase
     }
 
     public static void main(String[] args) {
-        minimalE2E();
+        initialize();
     }
 }
