3932ad178262c3bfcf5228eb89ba8642de79efd1
Vincent <yc660@duke.edu>
2025-03-22 12:29:56 -0400

    more fixes

--- app/src/test/java/productsimulation/model/RecipeTest.java
+++ app/src/test/java/productsimulation/model/RecipeTest.java
@@ -1,31 +1,23 @@
 package productsimulation.model;
 
-
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 import org.junit.jupiter.api.Test;
 
-public class FactoryTypeTest {
+public class RecipeTest {
   @Test
-  public void test_getName() {
-    FactoryType t = new FactoryType("type1", new ArrayList<>());
-    assertEquals("type1", t.getName());
+  public void test_getters() {
+    Recipe r = new Recipe("hotpot", 1, Collections.emptyMap()); 
+    assertEquals("hotpot", r.getName());
+    assertEquals(1, r.getLatency());
+    assertEquals(Collections.emptyMap(),r.getIngredients());
   }
 
   @Test
   public void test_toString(){
-    ArrayList<Recipe> recipes = new ArrayList<>();
-    recipes.add(new Recipe("out1", 3, Collections.emptyMap()));
-    recipes.add(new Recipe("out2", 2, Collections.emptyMap()));
-    FactoryType t = new FactoryType("type1", recipes);
-    String expected = "Factory Type\n{name='type1',\n recipes=[out1, out2]\n}";
-    assertEquals(expected, t.toString());
-
-    FactoryType t2 = new FactoryType("type2", Collections.emptyList());
-    String expected2 = "Factory Type\n{name='type2',\n recipes=[]\n}";
-    assertEquals(expected2, t2.toString());
+    Recipe r = new Recipe("hotpot", 1, Collections.emptyMap()); 
+    String expected = "Recipe\n{output='hotpot',\n ingredients={},\n latency=1\n}";
+    assertEquals(expected, r.toString());
   }
 }
 
