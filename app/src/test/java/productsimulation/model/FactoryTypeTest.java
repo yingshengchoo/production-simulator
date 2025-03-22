932402a3e3a766325dd1f73cfd2d709452d065c0
Vincent <yc660@duke.edu>
2025-03-22 12:42:02 -0400

    adding packages

--- app/src/test/java/productsimulation/model/FactoryTypeTest.java
+++ app/src/test/java/productsimulation/model/FactoryTypeTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import productsimulation.model.*;
 import org.junit.jupiter.api.Test;
 
 public class FactoryTypeTest {
