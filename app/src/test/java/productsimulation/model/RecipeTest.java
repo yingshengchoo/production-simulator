d665504971552b7599dbe15c4bde55793f2ef934
Vincent <yc660@duke.edu>
2025-03-27 00:53:12 -0400

    adding recipe tests

--- app/src/test/java/productsimulation/model/RecipeTest.java
+++ app/src/test/java/productsimulation/model/RecipeTest.java
@@ -19,6 +19,24 @@ public class RecipeTest {
     String expected = "Recipe\n{output='hotpot',\n ingredients={},\n latency=1\n}";
     assertEquals(expected, r.toString());
   }
+
+  @Test
+  public void test_set_and_getRecipeList(){
+    ArrayList<Recipe> list = new ArrayList<>();
+    Recipe r1 = new Recipe(12, Collections.emptyMap(), "recipe1");
+    Recipe r2 = new Recipe(7, Collections.emptyMap(), "recipe2");
+    Recipe r3 = new Recipe(2, Collections.emptyMap(), "recipe3");
+
+    list.add(r1);
+    list.add(r2);
+    list.add(r3);
+
+    Recipe r4 = new Recipe(1, Collections.emptyMap());
+
+    Recipe.setRecipeList(list);
+    assertEquals(r1, Recipe.getRecipe("recipe1"));
+    
+  }
 }
 
 
