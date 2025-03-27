da249916d724e6c3590fd593fc1339a7a33624a3
Vincent <yc660@duke.edu>
2025-03-27 00:14:37 -0400

    Request broadcaster tests

--- app/src/test/java/productsimulation/RequestBroadcasterTest.java
+++ app/src/test/java/productsimulation/RequestBroadcasterTest.java
@@ -2,12 +2,36 @@ package productsimulation;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import productsimulation.model.*;
+
 import org.junit.jupiter.api.Test;
+import java.util.Collections;
 
 public class RequestBroadcasterTest {
   @Test
   public void test_() {
+    RequestBroadcaster rb = RequestBroadcaster.getInstance();
+    assertEquals(0, rb.getBuildingsSize());
+    assertEquals(0, rb.getRecipesSize());
+    Recipe r1 = new Recipe(1, Collections.emptyMap(), "hotpot");
+    Recipe r2 = new Recipe(3, Collections.emptyMap(), "chocolate");
+    rb.addRecipes(r1);
+    rb.addRecipes(r2);
+    FactoryType t1 = new FactoryType("type1", Collections.emptyMap());
+    Factory f1 =  new Factory("factory1", t1, Collections.emptyList(), null, null);
+    Factory f2 = new Factory("factory1", t1, Collections.emptyList(), null, null);
+    Mine m1 = new Mine("mine1", new FactoryType("minetype", Collections.emptyMap()), null, null);
+    rb.addBuildings(f1);
+    rb.addBuildings(f2);
+    rb.addBuildings(m1);
+    assertEquals(2, rb.getRecipesSize());
+    assertEquals(3, rb.getBuildingsSize());
 
+    rb.removeRecipes(r2);
+    rb.removeBuildings(f2);
+    assertEquals(1, rb.getRecipesSize());
+    assertEquals(2, rb.getBuildingsSize());
+    
   }
 
 }
