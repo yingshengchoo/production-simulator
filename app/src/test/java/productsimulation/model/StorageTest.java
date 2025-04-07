37cef67b9b3db246e09620ed1004b7b68b05a9dd
Vincent <yc660@duke.edu>
2025-04-07 19:47:51 -0400

    fixed testcase

--- app/src/test/java/productsimulation/model/StorageTest.java
+++ app/src/test/java/productsimulation/model/StorageTest.java
@@ -80,9 +80,7 @@ public class StorageTest {
    assertEquals(100, s1.getPriority());
    assertEquals(100, s1.getTotalCapacity());
    s1.updateFrequency();
-   assertEquals(1, (int)Math.ceil((double)(s1.getTotalCapacity() * s1.getTotalCapacity()) / (double)(s1.getR() * s1.getPriority())));
-   assertEquals((int)Math.ceil((s1.getTotalCapacity() * s1.getTotalCapacity()) / (s1.getR() * s1.getPriority())), s1.getFrequency());
-   assertEquals(100, s1.getTotalCapacity());
+   assertEquals(1, s1.getFrequency());
    assertEquals(1, f.getRequestCount());
    assertEquals(2, s1.getRequestCount());
    assertEquals(0, s1.getStockCount());
@@ -91,7 +89,14 @@ public class StorageTest {
    
    t.stepNHandler(1);
    assertEquals(1, t.getStep());
-   
+   assertEquals(100, s1.getR());
+   s1.updateFrequency();
+   assertEquals(1, s1.getFrequency());
+   assertEquals(1, f.getRequestCount());
+   assertEquals(2, s1.getRequestCount());
+   assertEquals(0, s1.getStockCount());
+   assertEquals(1, m1.getRequestCount());
+   assertEquals(0, m2.getRequestCount());
    
    t.stepNHandler(1);
    assertEquals(2, t.getStep());
