1a7279f6fa8fd944f98d538a90b85ffc58afe7a9
Vincent <yc660@duke.edu>
2025-04-08 00:28:48 -0400

    fixed time

--- app/src/test/java/productsimulation/model/StorageTest.java
+++ app/src/test/java/productsimulation/model/StorageTest.java
@@ -126,7 +126,7 @@ public class StorageTest {
    assertEquals(1, s1.getStorage().size());
 
    t.stepNHandler(1);
-   assertEquals(3, t.getStep());
+   assertEquals(4, t.getStep());
    assertEquals(1, s1.getFrequency());
    assertEquals(1, f.getRequestCount());
    assertEquals(0, s1.getRequestCount());
