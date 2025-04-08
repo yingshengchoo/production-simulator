718b8fd6f6bf28a05e4e8d234928925bb7dcf9ee
Vincent <yc660@duke.edu>
2025-04-07 20:12:34 -0400

    check what gets the request

--- app/src/test/java/productsimulation/model/StorageTest.java
+++ app/src/test/java/productsimulation/model/StorageTest.java
@@ -92,6 +92,7 @@ public class StorageTest {
    assertEquals(101, s1.getR());
    s1.updateFrequency();
    assertTrue(LogicTime.getInstance().getStep() % s1.getFrequency() == 0);
+   assertEquals(m1, s1.getSourcePolicy().getSource(sources, socks.getOutput()));
    assertEquals(1, s1.getFrequency());
    assertEquals(1, f.getRequestCount());
    assertEquals(2, s1.getRequestCount());
