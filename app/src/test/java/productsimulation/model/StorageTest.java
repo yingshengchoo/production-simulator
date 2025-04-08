e7caf851bf3c52bca5e16d3261237b9408df0fc5
Vincent <yc660@duke.edu>
2025-04-08 00:30:04 -0400

    fixed frequency

--- app/src/test/java/productsimulation/model/StorageTest.java
+++ app/src/test/java/productsimulation/model/StorageTest.java
@@ -127,7 +127,7 @@ public class StorageTest {
 
    t.stepNHandler(1);
    assertEquals(4, t.getStep());
-   assertEquals(1, s1.getFrequency());
+   assertEquals(2, s1.getFrequency());
    assertEquals(1, f.getRequestCount());
    assertEquals(0, s1.getRequestCount());
    assertEquals(0, s1.getStockCount());
