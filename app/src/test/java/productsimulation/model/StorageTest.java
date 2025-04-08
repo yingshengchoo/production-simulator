597ece47aefd675604cf0fcd629bb0f8b60834fc
Vincent <yc660@duke.edu>
2025-04-08 00:43:34 -0400

    fixing test error

--- app/src/test/java/productsimulation/model/StorageTest.java
+++ app/src/test/java/productsimulation/model/StorageTest.java
@@ -130,7 +130,7 @@ public class StorageTest {
    assertEquals(2, s1.getFrequency());
    assertEquals(1, f.getRequestCount());
    assertEquals(-1, s1.getRequestCount());
-   assertEquals(0, s1.getStockCount());
+   assertEquals(1, s1.getStockCount());
    assertEquals(1, m1.getRequestCount());
    assertEquals(0, m2.getRequestCount());
    assertEquals(0, s1.getStorage().size());
