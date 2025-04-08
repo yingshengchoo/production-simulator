9770718bf1619c6873e94170382477554f23ee2a
Vincent <yc660@duke.edu>
2025-04-07 23:47:14 -0400

    retry

--- app/src/test/java/productsimulation/model/StorageTest.java
+++ app/src/test/java/productsimulation/model/StorageTest.java
@@ -108,7 +108,7 @@ public class StorageTest {
    assertEquals(100, s1.getR());
    assertTrue(LogicTime.getInstance().getStep() % s1.getFrequency() == 0); 
    assertEquals(1, s1.getFrequency());
-   assertEquals(0, f.getRequestCount());
+   assertEquals(1, f.getRequestCount());
    assertEquals(2, s1.getRequestCount());
    assertEquals(0, s1.getStockCount());
    assertEquals(0, m1.getRequestCount());
