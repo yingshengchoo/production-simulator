581de486678563ca1ecfe2a566548ffa51e2018a
Vincent <yc660@duke.edu>
2025-04-18 18:59:08 -0400

    fixing more tests

--- app/src/test/java/productsimulation/model/BuildingCostHandlerTest.java
+++ app/src/test/java/productsimulation/model/BuildingCostHandlerTest.java
@@ -89,11 +89,13 @@ public class BuildingCostHandlerTest {
     //Here, mine should be 1 (as storage automatically request from mine since 0%100 = 0
     assertEquals(1, m1.getRequestCount());
     assertEquals(0, s1.getReqCount());
+    
+    t.stepNHandler(1);  
 
-    method.invoke(null, "wood", 2);
+    method.invoke(null, "wood", 1);
    
-    assertEquals(3, m1.getRequestCount());
-    assertEquals(1, s1.getReqCount());
+    assertEquals(1, m1.getRequestCount());
+    assertEquals(0, s1.getReqCount());
 
     assertEquals(4, GlobalStorage.getItemCount("wood")); //Stilling in readyQueue
     
